c7df4b6e51685fe2d148fa84bb45daa93f68a188
Justin <justincsing@gmail.com>
2024-04-25 08:48:45 -0400

    add: bokeh plotting code for dataframe

--- pyopenms_viz/ChromatogramPlotter.py
+++ pyopenms_viz/ChromatogramPlotter.py
@@ -1,22 +1,83 @@
 from .BasePlotter import _BasePlotter, _BasePlotterConfig
+from pandas import DataFrame
 from pyopenms import MSChromatogram
 from dataclasses import dataclass
 from typing import Literal
 
 @dataclass(kw_only=True)
 class ChromatogramPlotterConfig(_BasePlotterConfig):
+    # Plot Aesthetics
     title: str = "Chromatogram Plot"
     xlabel: str = "Retention Time"
     ylabel: str = "Intensity"
+    show_legend: bool = True
+    show_plot: bool = True
+    
+    # Data Specific Attributes
     ion_mobility: bool = False # if True, plot ion mobility as well in a heatmap
 
 class ChromatogramPlotter(_BasePlotter):
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
     
-    def _plotBokeh(self, chromatogram: MSChromatogram):
-        ##TODO
-        pass
+    def _plotBokeh(self, data: DataFrame):
+        from bokeh.plotting import figure, show
+        from bokeh.models import ColumnDataSource, Legend
+        from bokeh.palettes import Category10
+
+        # Tooltips for interactive information
+        TOOLTIPS = [
+                ("index", "$index"),
+                ("Retention Time", "@rt{0.2f}"),
+                ("Intensity", "@int{0.2f}"),
+                ("m/z", "@mz{0.4f}")
+            ]
+        
+        if "Annotation" in data.columns:
+            TOOLTIPS.append(("Annotation", "@Annotation"))
+        else:
+            TOOLTIPS.append(("Annotation", "Unknown"))
+            data["Annotation"] = "Unknown"
+        if "product_mz" in data.columns:
+            TOOLTIPS.append(("Target m/z", "@product_mz{0.4f}"))
+        
+        # Create the Bokeh plot
+        p = figure(title="Chromatogram", x_axis_label="Retention Time (min)", y_axis_label="Intensity", plot_width=self.config.width, plot_height=self.config.height, tooltips=TOOLTIPS)
+
+        # Create a legend
+        legend = Legend()
+
+        # Create a list to store legend items
+        legend_items = []
+
+        colors = Category10[len(data["Annotation"].unique())]
+        i = 0
+        for annotation, group_df in data.groupby('Annotation'):
+            source = ColumnDataSource(group_df)
+            line = p.line(x="rt", y="int", source=source, line_width=2, line_color=colors[i], line_alpha=0.5, line_dash='solid')
+            legend_items.append((annotation, [line]))
+            i+=1
+
+        # Add legend items to the legend
+        legend.items = legend_items
+
+        # Add the legend to the plot
+        p.add_layout(legend, 'right')
+
+        p.legend.location = "top_left"
+        # p.legend.click_policy="hide"
+        p.legend.click_policy="mute"
+        p.legend.title = "Transition"
+        p.legend.label_text_font_size = "10pt"
+
+        # Customize the plot
+        p.grid.visible = True
+        p.toolbar_location = "above"
+        
+        if self.config.show_plot:
+            show(p)
+        else:
+            return p
 
     def _plotPlotly(self, chromatogram: MSChromatogram):
         ##TODO
