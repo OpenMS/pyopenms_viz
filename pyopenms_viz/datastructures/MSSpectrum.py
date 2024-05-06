7f9172245851f86d2f3068783e07ebe6c2bedca9
singjc <justincsing@gmail.com>
2024-05-06 11:06:46 -0400

    move: native id and ms_level to optional

--- pyopenms_viz/datastructures/MSSpectrum.py
+++ pyopenms_viz/datastructures/MSSpectrum.py
@@ -3,13 +3,13 @@ Schema definition for mass spectrometry spectrum.
 """
 
 REQUIRED_SPECTRUM_DATAFRAME_COLUMNS = {
-    "native_id": "String column representing the native identifier (i.e. scan number) of the spectrum.",
     "mz": "Numeric column representing the mass-to-charge ratio (m/z) values of the peaks in the mass spectrum.",
-    "intensity": "Numeric column representing the intensity (abundance) of the peaks in the mass spectrum.",
-    "ms_level": "Integer column indicating the MS level (1 for MS1, 2 for MS2, etc.)."
+    "intensity": "Numeric column representing the intensity (abundance) of the peaks in the mass spectrum."
 }
 
 OPTIONAL_METADATA_SPECTRUM_DATAFRAME_COLUMNS = {
+    "native_id": "String column representing the native identifier (i.e. scan number) of the spectrum.",
+    "ms_level": "Integer column indicating the MS level (1 for MS1, 2 for MS2, etc.).",
     "sequence": "String column representing the peptide sequence.",
     "modified_sequence": "String column representing the modified peptide sequence.",
     "precursor_mz": "Numeric column representing the mass-to-charge ratio (m/z) of the precursor ion.",
