7f2e37de80bfc8e3bf2cb48ee3552fcd0caf678b
singjc <justincsing@gmail.com>
2024-05-06 11:01:57 -0400

    add: clarify modiication naming convention

--- pyopenms_viz/datastructures/MSChromatogram.py
+++ pyopenms_viz/datastructures/MSChromatogram.py
@@ -12,7 +12,7 @@ OPTIONAL_METADATA_CHROMATOGRAM_DATAFRAME_COLUMNS = {
     "chromatogram_type": "Type of chromatogram must be one of: MASS_CHROMATOGRAM, TOTAL_ION_CURRENT_CHROMATOGRAM,  SELECTED_ION_CURRENT_CHROMATOGRAM, BASEPEAK_CHROMATOGRAM, SELECTED_ION_MONITORING_CHROMATOGRAM, SELECTED_REACTION_MONITORING_CHROMATOGRAM, ELECTROMAGNETIC_RADIATION_CHROMATOGRAM, ABSORPTION_CHROMATOGRAM, EMISSION_CHROMATOGRAM"
     "ms_level": "Integer column indicating the MS level (1 for MS1, 2 for MS2, etc.)."
     "sequence": "String column representing the peptide sequence.",
-    "modified_sequence": "String column representing the modified peptide sequence.",
+    "modified_sequence": "String column representing the modified peptide sequence. Modification can be represented using the UniMod ontology, either with the UniMod Accession (UniMod: 21) or with the UniMod Codename (Phospho).",
     "precursor_mz": "Numeric column representing the mass-to-charge ratio (m/z) of the precursor ion.",
     "precursor_charge": "Integer column representing the charge state of the precursor ion.",
     "product_mz": "Numeric column representing the mass-to-charge ratio (m/z) of the product ion.",
