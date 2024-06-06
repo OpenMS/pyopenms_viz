5a6840dd2f124cc2bb9b60355d3e0e80d64753fc
axelwalter <axel.walter@uni-tuebingen.de>
2024-06-06 13:21:02 +0200

    add help text to parameter widgets

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ def get_common_parameters():
     params = {}
     with st.sidebar:
         params["engine"] = st.selectbox("engine", ["MATPLOTLIB", "BOKEH", "PLOTLY"])
+        params["relative_intensity"] = st.checkbox("relative_intensity", False, help="If true, plot relative intensity values. Defaults to False.")
         params["width"] = st.number_input("width", 50, 1000, 500, 50)
         params["height"] = st.number_input("height", 50, 1000, 500, 50)
         params["title"] = st.text_input("title", "Title")
@@ -46,11 +47,10 @@ def get_common_parameters():
 
 def get_MSExperiment_params():
     params = {}
-    params["plot3D"] = st.checkbox("plot3D", False)
-    params["relative_intensity"] = st.checkbox("relative_intensity", False)
-    params["bin_peaks"] = st.selectbox("bin_peaks", ["auto", "True", "False"])
-    params["num_RT_bins"] = st.number_input("num_RT_bins", 10, 100, 50, 10)
-    params["num_mz_bins"] = st.number_input("num_mz_bins", 10, 100, 50, 10)
+    params["plot3D"] = st.checkbox("plot3D", False, help="Plot peak map 3D with peaks colored based on intensity. Disables colorbar legend. Works with 'MATPLOTLIB'' engine only. Defaults to False.")
+    params["bin_peaks"] = st.selectbox("bin_peaks", ["auto", "True", "False"], help="Bin peaks to reduce complexity and improve plotting speed. Hovertext disabled if activated. If set to 'auto' any MSExperiment with more then num_RT_bins x num_mz_bins peaks will be binned. Defaults to 'auto'.")
+    params["num_RT_bins"] = st.number_input("num_RT_bins", 10, 100, 50, 10, help="Number of bins in RT dimension. Defaults to 50.")
+    params["num_mz_bins"] = st.number_input("num_mz_bins", 10, 100, 50, 10, help="Number of bins in m/z dimension. Defaults to 50.")
     if params["bin_peaks"] == "True":
         params["bin_peaks"] = True
     elif params["bin_peaks"] == "False":
@@ -59,15 +59,14 @@ def get_MSExperiment_params():
 
 def get_Spectrum_params():
     params = {}
-    params["ion_mobility"] = st.checkbox("ion_mobility")
-    params["annotate_mz"] = st.checkbox("annotate_mz")
-    params["annotate_ions"] = st.checkbox("annotate_ions")
-    params["annotate_sequence"] = st.checkbox("annotate_sequence")
-    params["mirror_spectrum"] = st.checkbox("mirror_spectrum")
-    params["relative_intensity"] = st.checkbox("relative_intensity", False)
-    params["custom_peak_color"] = st.checkbox("custom_peak_color")
-    params["custom_annotation_text"] = st.checkbox("custom_annotation_text")
-    params["custom_annotation_color"] = st.checkbox("custom_annotation_color")
+    params["ion_mobility"] = st.checkbox("ion_mobility", help="If true, plots spectra (not including reference spectra) as heatmap of m/z vs ion mobility with intensity as color. Defaults to False.")
+    params["annotate_mz"] = st.checkbox("annotate_mz", help="If true, annotate peaks with m/z values. Defaults to False.")
+    params["annotate_ions"] = st.checkbox("annotate_ions", help="If true, annotate fragment ions. Defaults to False.")
+    params["annotate_sequence"] = st.checkbox("annotate_sequence", help="Annotate peaks based on sequence provided. Defaults to False")
+    params["mirror_spectrum"] = st.checkbox("mirror_spectrum", help="If true, plot mirror spectrum. Defaults to True, if no mirror reference_spectrum is provided, this is ignored.")
+    params["custom_peak_color"] = st.checkbox("custom_peak_color", help="If true, plot peaks with colors from 'color_peak' column.")
+    params["custom_annotation_text"] = st.checkbox("custom_annotation_text", help="If true, annotate peaks with custom text from 'custom_annotation' column. Overwrites all other annotations.Use <br> for line breaks.")
+    params["custom_annotation_color"] = st.checkbox("custom_annotation_color", help="If true, plot annotations with colors from 'color_annotation' column.")
     return params
 
 if "exp_df" not in st.session_state:
