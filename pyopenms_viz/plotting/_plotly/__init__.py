83c4d7a52b132d70f773ac677af922d8915b5a88
singjc <justincsing@gmail.com>
2024-06-17 01:43:42 -0400

    add plotly chromatogram plot method

--- pyopenms_viz/plotting/_plotly/__init__.py
+++ pyopenms_viz/plotting/_plotly/__init__.py
@@ -3,14 +3,26 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from pyopenms_viz.plotting._plotly.core import (
-    
+    LinePlot,
+    VLinePlot,
+    ScatterPlot,
+    ChromatogramPlot,
+    MobilogramPlot,
+    SpectrumPlot,
+    FeatureHeatmapPlot
 )
 
 if TYPE_CHECKING:
     from pyopenms_viz.plotting._plotly.core import PLOTLYPlot
     
 PLOT_CLASSES: dict[str, type[PLOTLYPlot]] = {
-    
+    "line": LinePlot,
+    "vline": VLinePlot,
+    "scatter": ScatterPlot,
+    "chromatogram": ChromatogramPlot,
+    "mobilogram": MobilogramPlot,
+    "spectrum": SpectrumPlot,
+    "feature_heatmap": FeatureHeatmapPlot
 }
 
 def plot(data, kind, **kwargs):
--- pyopenms_viz/plotting/_plotly/core.py
+++ pyopenms_viz/plotting/_plotly/core.py
@@ -7,12 +7,15 @@ from abc import (
 
 from typing import TYPE_CHECKING, Literal, List, Tuple, Union
 
+import plotly.graph_objects as go
 from plotly.graph_objs import Figure
 
 from pandas.core.frame import DataFrame
 from pandas.errors import AbstractMethodError
 from pandas import Index
 from pandas.core.dtypes.common import is_integer
+
+from numpy import column_stack
 
 from pyopenms_viz.plotting._config import (
     SpectrumPlotterConfig,
@@ -144,7 +147,7 @@ class PLOTLYPlot(ABC):
             
         if fig is None:
             self.fig = go.Figure()
-            fig.update_layout(
+            self.fig.update_layout(
                 title=self.title,
                 xaxis_title=self.xlabel,
                 yaxis_title=self.ylabel,
@@ -155,6 +158,98 @@ class PLOTLYPlot(ABC):
         if self.by is not None:
             # Ensure by column data is string
             self.data[self.by] = self.data[self.by].astype(str)
+    
+    def _make_plot(self, fig: Figure, **kwargs) -> Figure:
+        raise AbstractMethodError(self)
+    
+    def _update_plot_aes(self, fig, **kwargs) -> None:
+        """
+        Update the plot aesthetics.
+        """
+        fig.update_layout(
+            legend_title = self.config.legend.title,
+            legend_font_size = self.config.legend.fontsize,
+            showlegend = self.config.legend.show
+        )
+        
+        # Update to look similar to Bokeh theme
+        # Customize the layout
+        fig.update_layout(
+            plot_bgcolor='#FFFFFF',  # Set the plot background color
+            font_family='Helvetica',  # Set the font family
+            font_size=12,  # Set the font size
+            title_font_family='Helvetica',  # Set the title font family
+            title_font_size=16,  # Set the title font size
+            xaxis_title_font_family='Helvetica',  # Set the x-axis title font family
+            xaxis_title_font_size=14,  # Set the x-axis title font size
+            yaxis_title_font_family='Helvetica',  # Set the y-axis title font family
+            yaxis_title_font_size=14,  # Set the y-axis title font size
+            xaxis_gridcolor='#CCCCCC',  # Set the x-axis grid color
+            yaxis_gridcolor='#CCCCCC',  # Set the y-axis grid color
+            xaxis_tickfont_family='Helvetica',  # Set the x-axis tick font family
+            yaxis_tickfont_family='Helvetica',  # Set the y-axis tick font family
+            legend_font_family='Helvetica',  # Set the legend font family
+        )
+        
+        # Add x-axis grid lines and ticks
+        fig.update_xaxes(
+            showgrid=self.config.grid,  # Add x-axis grid lines
+            showline=True, 
+            linewidth=1, 
+            linecolor='black',
+            ticks='outside',  # Add x-axis ticks outside the plot area
+            tickwidth=1,  # Set the width of x-axis ticks
+            tickcolor='black',  # Set the color of x-axis ticks
+            # ticklen=5,  # Set the length of x-axis ticks
+        )
+
+        # Add y-axis grid lines and ticks
+        fig.update_yaxes(
+            showgrid=self.config.grid,  # Add y-axis grid lines
+            showline=True, 
+            linewidth=1, 
+            linecolor='black',
+            # ticks='outside',  # Add y-axis ticks outside the plot area
+            tickwidth=1,  # Set the width of y-axis ticks
+            tickcolor='black',  # Set the color of y-axis ticks
+            # ticklen=5,  # Set the length of y-axis ticks
+        )
+            
+    def _add_legend(self, fig, legend):
+        pass
+    
+    def _add_tooltips(self, fig, tooltips, custom_hover_data):
+        fig.update_traces(
+            hovertemplate=tooltips,
+            customdata=custom_hover_data
+        )
+    
+    def _add_bounding_box_drawer(self, fig, **kwargs):
+        pass
+    
+    def _add_boundingvertical_drawer(self, fig, **kwargs):
+        pass
+    
+    def _modify_x_range(self, x_range: Tuple[float, float] | None = None, padding: Tuple[float, float] | None = None):
+        start, end = x_range
+        if padding is not None:
+            start = start - (start * padding[0])
+            end = end + (end * padding[1])
+        self.fig.update_xaxes(range=[start, end])
+    
+    def _modify_y_range(self, y_range: Tuple[float, float] | None = None, padding: Tuple[float, float] | None = None):
+        start, end = y_range
+        if padding is not None:
+            start = start - (start * padding[0])
+            end = end + (end * padding[1])
+        self.fig.update_yaxes(range=[start, end])
+    
+    def generate(self, **kwargs):
+        self._make_plot(self.fig, **kwargs)
+        return self.fig
+    
+    def show(self, **kwargs):
+        self.fig.show()
 
 
 class PlanePlot(PLOTLYPlot, ABC):
@@ -175,3 +270,139 @@ class PlanePlot(PLOTLYPlot, ABC):
             
         self.x = x
         self.y = y
+      
+  
+class LinePlot(PlanePlot):
+    """
+    Class for assembling a Plotly line plot
+    """
+    
+    @property
+    def _kind(self) -> Literal["line", "vline", "chromatogram"]:
+        return "line"
+    
+    def __init__(self, data, x, y, **kwargs) -> None:
+        super().__init__(data, x, y, **kwargs)
+        
+    def _make_plot(self, fig: Figure, **kwargs) -> Figure:
+        # Check for tooltips in kwargs and pop
+        tooltips = kwargs.pop("tooltips", None)
+        custom_hover_data = kwargs.pop("custom_hover_data", None)
+        
+        traces = self._plot(self.data, self.x, self.y, self.by, **kwargs)
+        fig.add_traces(data=traces)
+        
+        self._update_plot_aes(self.fig)
+        
+        if tooltips is not None:
+            self._add_tooltips(fig, tooltips, custom_hover_data)
+        
+    
+    @classmethod
+    def _plot( # type: ignore[override]
+        cls,
+        data: DataFrame,
+        x: Union[str, int],
+        y: Union[str, int],
+        by: str | None = None,
+        **kwargs
+    ) -> Figure:
+        color_gen = kwargs.pop("line_color", None)
+        
+        traces = []
+        if by is None:
+            trace = go.Scatter(
+                x=data[x],
+                y=data[y],
+                mode="lines",
+                line=dict(
+                    color=next(color_gen)
+                    )
+            )
+            traces.append(trace)
+        else:
+            for group, df in data.groupby(by):
+                trace = go.Scatter(
+                    x=df[x],
+                    y=df[y],
+                    mode="lines",
+                    name=group,
+                    line=dict(
+                        color=next(color_gen)
+                    )
+                )
+                traces.append(trace)
+                
+        return traces
+
+
+class VLinePlot(LinePlot):
+    pass
+
+
+class ScatterPlot(PlanePlot):
+    pass
+
+
+class ChromatogramPlot(LinePlot):
+    
+    @property
+    def _kind(self) -> Literal["chromatogram"]:
+        return "chromatogram"
+
+    def __init__(self, data, x, y, feature_data: DataFrame | None = None, **kwargs) -> None:
+        if "config" not in kwargs or kwargs["config"] is None:
+            kwargs["config"] = ChromatogramPlotterConfig()
+            
+        super().__init__(data, x, y, **kwargs)
+        
+        self.feature_data = feature_data
+        
+        self.plot()
+        if self.show_plot:
+            self.show()
+            
+    def plot(self, **kwargs):
+        
+        color_gen = ColorGenerator()
+        
+        available_columns = self.data.columns.tolist()
+        # Get index (not a column in dataframe) data first for customer hover data
+        custom_hover_data = [self.data.index]
+        # Get the rest of the columns
+        custom_hover_data  += [self.data[col] for col in ["mz", "Annotation", "product_mz"] if col in available_columns]
+        
+        
+        TOOLTIPS = [
+            "Index: %{customdata[0]}",
+            "Retention Time: %{x:.2f}",
+            "Intensity: %{y:.2f}",
+        ]
+        
+        custom_hover_data_index = 1
+        if "mz" in self.data.columns:
+            TOOLTIPS.append("m/z: %{customdata[" + str(custom_hover_data_index) + "]:.4f}")
+            custom_hover_data_index += 1
+        if "Annotation" in self.data.columns:
+            TOOLTIPS.append("Annotation: %{customdata[" + str(custom_hover_data_index) + "]}")
+            custom_hover_data_index += 1
+        if "product_mz" in self.data.columns:
+            TOOLTIPS.append("Target m/z: %{customdata[" + str(custom_hover_data_index) + "]:.4f}")
+        
+        self.fig = super().generate(line_color=color_gen, tooltips="<br>".join(TOOLTIPS), custom_hover_data=column_stack(custom_hover_data))
+        
+        self._modify_y_range((0, self.data[self.y].max()), (0, 0.1))
+        
+        
+
+class MobilogramPlot(ChromatogramPlot):
+    pass
+
+
+class SpectrumPlot(VLinePlot):
+    pass
+
+
+class FeatureHeatmapPlot(ScatterPlot):
+    pass
+
