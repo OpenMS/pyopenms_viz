0c7ae28eb013e71f3a06e52cb24c7cf71842edee
singjc <justincsing@gmail.com>
2024-06-18 16:35:08 -0400

    update: matplotlib init

--- pyopenms_viz/plotting/_matplotlib/__init__.py
+++ pyopenms_viz/plotting/_matplotlib/__init__.py
@@ -3,14 +3,26 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from pyopenms_viz.plotting._matplotlib.core import (
-
+    LinePlot,
+    VLinePlot,
+    ScatterPlot,
+    ChromatogramPlot,
+    MobilogramPlot,
+    SpectrumPlot,
+    FeatureHeatmapPlot
 )
 
 if TYPE_CHECKING:
     from pyopenms_viz.plotting._matplotlib.core import MATPLOTLIBPlot
     
 PLOT_CLASSES: dict[str, type[MATPLOTLIBPlot]] = {
-        
+        "line": LinePlot,
+        "vline": VLinePlot,
+        "scatter": ScatterPlot,
+        "chromatogram": ChromatogramPlot,
+        "mobilogram": MobilogramPlot,
+        "spectrum": SpectrumPlot,
+        "feature_heatmap": FeatureHeatmapPlot
     }
 
 def plot(data, kind, **kwargs):
