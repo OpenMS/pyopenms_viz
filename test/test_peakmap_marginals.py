330fddde10ed18fbf243304d6ca11a998b0748ab
Joshua Charkow <joshuacharkow@gmail.com>
2024-12-17 12:33:22 -0500

    new snapshot better than old
    found bug in the old snapshot with coloring that fixed

--- test/test_peakmap_marginals.py
+++ test/test_peakmap_marginals.py
@@ -13,6 +13,9 @@ def test_peakmap_marginals(featureMap_data, snapshot):
         y="rt",
         z="int",
         kind="peakmap",
+        xlabel="mass-to-charge",
+        ylabel="Retention Time",
+        zlabel="Intensity",
         add_marginals=True,
         show_plot=False,
     )
@@ -33,6 +36,9 @@ def test_peakmap_mz_im(featureMap_data, snapshot):
         add_marginals=True,
         x_kind="chromatogram",
         y_kind="chromatogram",
+        xlabel="Retention Time",
+        ylabel="Ion Mobility",
+        zlabel="Intensity",
         kind="peakmap",
         show_plot=False,
     )
