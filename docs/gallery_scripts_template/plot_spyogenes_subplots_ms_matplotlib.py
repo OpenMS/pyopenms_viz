93af84582a669e44d5e4abae2bed271d5d7e21fc
Nishant Garg <156602691+Nishantrde@users.noreply.github.com>
2025-03-30 08:51:28 +0530

    Simplifying MultiPlot Interface #83-resolving_conflick-7

--- docs/gallery_scripts_template/plot_spyogenes_subplots_ms_matplotlib.py
+++ docs/gallery_scripts_template/plot_spyogenes_subplots_ms_matplotlib.py
@@ -10,7 +10,6 @@ import requests
 import zipfile
 import matplotlib.pyplot as plt
 import sys
-import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
